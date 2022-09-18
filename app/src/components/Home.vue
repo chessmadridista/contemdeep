724264a2fd38d18c5cf942020499a795ed641c6e
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-19 00:31:31 +0530

    Add computed methods for dynamically calculating the labels.

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -1,15 +1,61 @@
 <template>
     <v-container>
-        <h2>
-            Hello, sample page here!
-        </h2>
+        <v-card>
+            <v-switch
+            v-model="isDoOrDont"
+            :label="calcSwitchLabel"></v-switch>
+            <v-card-text>
+                <v-textarea
+                outlined
+                :label="calcQuestionLabel"
+                >
+                </v-textarea>
+                <v-textarea
+                outlined
+                label="Because"
+                >
+                </v-textarea>
+            </v-card-text>
+        </v-card>
     </v-container>
 </template>
 <script>
 export default {
     name: "Home",
     data() {
-        return {};
+        return {
+            isDoOrDont: true,
+        };
+    },
+    computed: {
+        calcSwitchLabel() {
+            let switchLabel;
+
+            switch (this.isDoOrDont) {
+                case true:
+                    switchLabel = "Do";
+                    break;
+                case false:
+                    switchLabel = "Don't";
+                    break;
+            }
+
+            return switchLabel;
+        },
+        calcQuestionLabel() {
+            let questionLabel;
+
+            switch (this.isDoOrDont) {
+                case true:
+                    questionLabel = "Why do I";
+                    break;
+                case false:
+                    questionLabel = "Why don't I";
+                    break;
+            }
+
+            return questionLabel;
+        },
     },
 }
 </script>
