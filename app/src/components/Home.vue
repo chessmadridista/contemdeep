74f72d3e68e026301d2d23a2e4856179125e6369
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-19 00:54:14 +0530

    Add placeholder dynamically for the answer.

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -8,11 +8,13 @@
                 <v-textarea
                 outlined
                 :label="calcQuestionLabel"
+                :placeholder="calcQuestionPlaceholder"
                 >
                 </v-textarea>
                 <v-textarea
                 outlined
                 label="Because"
+                :placeholder="calcAnswerPlaceholder"
                 >
                 </v-textarea>
             </v-card-text>
@@ -55,6 +57,34 @@ export default {
             }
 
             return questionLabel;
+        },
+        calcQuestionPlaceholder() {
+            let questionPlaceholder;
+
+            switch (this.isDoOrDont) {
+                case true:
+                    questionPlaceholder = "Why do I procrastinate so much even though I have important stuff to do?";
+                    break;
+                case false:
+                    questionPlaceholder = "Why don't I smile at strangers?";
+                    break;
+            }
+
+            return questionPlaceholder;
+        },
+        calcAnswerPlaceholder() {
+            let answerPlaceholder;
+
+            switch (this.isDoOrDont) {
+                case true:
+                    answerPlaceholder = "Because I am overwhelmed by the responsibilities and I am afraid of failure.";
+                    break;
+                case false:
+                    answerPlaceholder = "Because I am afraid of rejection. Sometimes they don't smile back at me.";
+                    break;
+            }
+
+            return answerPlaceholder;
         },
     },
 }
