1ad19e8d302c39385f0ef8ced9ae54c660be42c0
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-27 22:54:26 +0530

    Add side navigation menu.

---
+++ app/src/components/NavigationHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container>
+        <v-app-bar
+        app>
+            <v-app-bar-nav-icon
+            @click="drawer = true"
+            ></v-app-bar-nav-icon>
+            <v-toolbar-title>Home</v-toolbar-title>
+        </v-app-bar>
+        <v-navigation-drawer
+        temporary
+        absolute
+        v-model="drawer">
+            <v-list-item-group>
+                <v-list nav>
+                    <v-list-item>
+                        <v-list-item-title>
+                            Home
+                        </v-list-item-title> 
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-title>
+                            Results
+                        </v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-list-item-group>
+        </v-navigation-drawer>
+    </v-container>
+</template>
+<script>
+export default {
+    name: "NavigationHeader",
+    data() {
+        return {
+            drawer: false,
+        };
+    },
+}
+</script>
