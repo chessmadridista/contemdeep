266da51e993cef536b7174c07cb74e4393e9bd4c
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-29 18:14:06 +0530

    Align side nav menu items in the middle of the container.

--- app/src/components/NavigationHeader.vue
+++ app/src/components/NavigationHeader.vue
@@ -13,14 +13,14 @@
         v-model="drawer">
             <v-list-item-group>
                 <v-list nav>
-                    <v-list-item>
+                    <v-list-item
+                    v-for="item of items"
+                    :key="item.itemID">
+                        <v-list-item-icon>
+                            <v-icon>{{ item.itemIcon }}</v-icon>
+                        </v-list-item-icon>
                         <v-list-item-title>
-                            Home
-                        </v-list-item-title> 
-                    </v-list-item>
-                    <v-list-item>
-                        <v-list-item-title>
-                            Results
+                            {{ item.itemTitle }}
                         </v-list-item-title>
                     </v-list-item>
                 </v-list>
@@ -34,7 +34,31 @@ export default {
     data() {
         return {
             drawer: false,
+            items: [
+                {
+                    itemID: 0,
+                    itemTitle: "Home",
+                    itemIcon: "mdi-view-dashboard"
+                },
+                {
+                    itemID: 1,
+                    itemTitle: "Results",
+                    itemIcon: "mdi-hand-heart"
+                },
+            ],
         };
     },
 }
 </script>
+<style lang="scss" scoped>
+.v-item-group {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 100%;
+
+    .v-list-item__title {
+        text-align: left;
+    }
+}
+</style>
